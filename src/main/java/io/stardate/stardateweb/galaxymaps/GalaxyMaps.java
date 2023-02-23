3064ba80a16b9e25f64677530364dc0a6c54ef02
San <istamarahsan@gmail.com>
2023-02-23 10:24:37 +0700

    handle optional

--- src/main/java/io/stardate/stardateweb/galaxymaps/GalaxyMaps.java
+++ src/main/java/io/stardate/stardateweb/galaxymaps/GalaxyMaps.java
@@ -3,22 +3,26 @@ package io.stardate.stardateweb.galaxymaps;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.stardate.stardateweb.WebComponent;
+import io.stardate.stardateweb.galaxymaps.models.GalaxyMap;
+import io.stardate.stardateweb.galaxymaps.models.GalaxyMapPreview;
+
+import java.util.function.Consumer;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class GalaxyMaps implements WebComponent {
-    
+
     private final GalaxyMapsData data;
-    
+
     private GalaxyMaps(GalaxyMapsData data) {
         this.data = data;
     }
-    
+
     public static GalaxyMaps create(GalaxyMapsData data) {
         return new GalaxyMaps(data);
     }
-    
+
     @Override
     public void configureRoutes(Javalin app) {
         app.routes(() -> {
@@ -42,15 +46,25 @@ public class GalaxyMaps implements WebComponent {
 
     public void getGalaxyMap(Context context) {
         var id = context.pathParam("galaxyMapId");
-        context.json(data.get(id));
+        var result = data.get(id);
+        result.map(this::respondMap).orElse(ctx -> ctx.status(404)).accept(context);
     }
 
     public void preview(Context context) {
         var id = context.pathParam("galaxyMapId");
-        context.json(data.getPreview(id));
+        var result = data.getPreview(id);
+        result.map(this::respondPreview).orElse(ctx -> ctx.status(404)).accept(context);
     }
 
     public void all(Context context) {
         context.json(data.allAsPreview());
+    }
+
+    private Consumer<Context> respondMap(GalaxyMap map) {
+        return (ctx -> ctx.json(map));
+    }
+
+    private Consumer<Context> respondPreview(GalaxyMapPreview preview) {
+        return (ctx -> ctx.json(preview));
     }
 }
